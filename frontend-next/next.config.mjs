50b620cb901e182013bcce2890eec5e7a1721f6e
JeanZorzetti <flow.controlx@gmail.com>
2025-11-21 17:34:22 -0300

    fix: update offline page and pwa config

--- frontend-next/next.config.mjs
+++ frontend-next/next.config.mjs
@@ -5,6 +5,9 @@ const withPWA = withPWAInit({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  fallbacks: {
+    document: '/offline.html',
+  },
 });
 
 /** @type {import('next').NextConfig} */
